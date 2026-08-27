29fd21edbbdcb14734cdc2de660585d53bcf81da
mmrfcmo <mrfcmo@gmail.com>
2026-08-27 13:09:12 +0100

    Add security

---
+++ app/core/security.py
@@ -0,0 +1,9 @@
+"""Security utilities."""
+from passlib.context import CryptContext
+from app.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
